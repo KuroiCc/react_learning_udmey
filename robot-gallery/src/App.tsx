763e16f4b8ecfd2cb35f4c4c44c2514eb202c6fb
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-13 02:59:38 +0900

    add App's css & apply it as a module

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import './App.css'
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <ul>
-      {robots.map((robot) => (
-        <Robot key={robot.id} email={robot.email} name={robot.name} />
-      ))}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robots.map((robot) => (
+          <Robot key={robot.id} id={robot.id} email={robot.email} name={robot.name} />
+        ))}
+      </div>
+    </div>
   )
 }
 
