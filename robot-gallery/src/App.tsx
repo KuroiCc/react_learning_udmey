87592610a1e86632376c52d3fa8f7359973ce1cf
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-14 15:40:24 +0900

    change: App to Class component

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -2,25 +2,47 @@ import React from 'react'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 
-import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="logo" className={styles.appLogo} />
-        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+
+interface State {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  robotsGallery: any[]
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      robotsGallery: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => this.setState({ robotsGallery: data }))
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="logo" className={styles.appLogo} />
+          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotsGallery.map((robot) => (
+            <Robot key={robot.id} id={robot.id} email={robot.email} name={robot.name} />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((robot) => (
-          <Robot key={robot.id} id={robot.id} email={robot.email} name={robot.name} />
-        ))}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
