74aaaf96d813677219aa38385b09ec3ac5117fe0
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-16 16:55:47 +0900

    change: CC to FC & loading, catch error

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,48 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Props {}
+const App: React.FC = () => {
+  const [robotsGallery, setRobotsGallery] = useState<any>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string>('')
 
-interface State {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  robotsGallery: any[]
-}
+  useEffect(() => {
+    setLoading(true)
 
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      robotsGallery: []
-    }
-  }
-
-  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((data) => this.setState({ robotsGallery: data }))
-  }
+      .then((data) => setRobotsGallery(data))
+      .catch((error) => setError(error.message))
 
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} alt="logo" className={styles.appLogo} />
-          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-        </div>
-        <ShoppingCart />
+    setLoading(false)
+  }, [])
+
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
+      <ShoppingCart />
+      {/* {(!error || error !== '') && <div>site error{error}</div>} */}
+      {error && <div>site error: {error}</div>}
+      {loading ? (
+        <h2>loading...</h2>
+      ) : (
         <div className={styles.robotList}>
-          {this.state.robotsGallery.map((robot) => (
+          {robotsGallery.map((robot: any) => (
             <Robot key={robot.id} id={robot.id} email={robot.email} name={robot.name} />
           ))}
         </div>
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
 
 export default App
