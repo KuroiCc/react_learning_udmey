fc6fa2a79985dbc3baa0847ee142ae4ecddbe851
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-19 17:21:12 +0900

    add top header

--- react-travel/src/components/header/Header.tsx
+++ react-travel/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Typography, Input, Layout } from 'antd'
+import { Typography, Input, Layout, Dropdown, Menu, Button } from 'antd'
+import { GlobalOutlined } from '@ant-design/icons'
 
 import styles from './Header.module.css'
 import logo from '../../assets/logo.svg'
@@ -7,6 +8,29 @@ import logo from '../../assets/logo.svg'
 export const Header: React.FC = () => {
   return (
     <div className={styles['app-header']}>
+      {/* top-header */}
+      <div className={styles['top-header']}>
+        <div className={styles.inner}>
+          <Typography.Text>让旅游更幸福</Typography.Text>
+          <Dropdown.Button
+            style={{ marginLeft: 15 }}
+            overlay={
+              <Menu>
+                <Menu.Item>中文</Menu.Item>
+                <Menu.Item>English</Menu.Item>
+              </Menu>
+            }
+            icon={<GlobalOutlined />}
+          >
+            语言
+          </Dropdown.Button>
+          <Button.Group className={styles['button-group']}>
+            <Button>注册</Button>
+            <Button>登陆</Button>
+          </Button.Group>
+        </div>
+      </div>
+
       {/* main-header */}
       <div>
         <Layout.Header className={styles['main-header']}>
