fa07f2d1a236be976c7586b43a60447d68be1c3f
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-20 19:35:46 +0900

    fix: Avoid duplication of keys

--- react-travel/src/components/sideMenu/SideMenu.tsx
+++ react-travel/src/components/sideMenu/SideMenu.tsx
@@ -10,10 +10,19 @@ export const SideMenu: React.FC = () => {
     <Menu mode="vertical" className={styles['side-menu']}>
       {sideMenuList.map((m, index) => (
         <Menu.SubMenu key={`side-menu-${index}`} icon={<GifOutlined />} title={m.title}>
-          {m.subMenu.map((sm, index) => (
-            <Menu.SubMenu key={`sub-menu-${index}`} title={sm.title}>
-              {sm.subMenu.map((sms, index) => (
-                <Menu.Item key={`sub-menu-item-${index}`}>{sms}</Menu.Item>
+          {m.subMenu.map((sm, smIndex) => (
+            <Menu.SubMenu
+              key={`sub-menu-${index}-${smIndex}`}
+              icon={<GifOutlined />}
+              title={sm.title}
+            >
+              {sm.subMenu.map((sms, smsIndex) => (
+                <Menu.Item
+                  key={`sub-menu-item-${index}-${smIndex}-${smsIndex}`}
+                  icon={<GifOutlined />}
+                >
+                  {sms}
+                </Menu.Item>
               ))}
             </Menu.SubMenu>
           ))}
