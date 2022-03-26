e43e22be076b9913fe30f9c9f878d0dfc5e9603c
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-26 16:44:43 +0900

    add header link

--- react-travel/src/components/productCollection/ProductImage.tsx
+++ react-travel/src/components/productCollection/ProductImage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Image, Typography } from 'antd'
+import { Link } from 'react-router-dom'
 
 interface PropsType {
   id: string | number
@@ -11,17 +12,17 @@ interface PropsType {
 
 export const ProductImage: React.FC<PropsType> = ({ id, size, imageSrc, price, title }) => {
   return (
-    <>
+    <Link to={`detail/${id}`}>
       {
         // prettier-ignore
         size === 'large'
-          ? (<Image src={imageSrc} alt={title} height={285} width={490} />)
-          : (<Image src={imageSrc} alt={title} height={120} width={240} />)
+          ? (<Image src={imageSrc} alt={title} height={285} width={490} preview={false} />)
+          : (<Image src={imageSrc} alt={title} height={120} width={240} preview={false} />)
       }
       <div>
         <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
         <Typography.Text type="danger">¥ {price} 起</Typography.Text>
       </div>
-    </>
+    </Link>
   )
 }
